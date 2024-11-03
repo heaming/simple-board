f96f463aa52749c986b9b25ff80fa23236e86fc5
heaming <meetoohi@gmail.com>
2024-11-03 22:10:26 +0900

    [2024-11-03] board dto 완료

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,7 +1,22 @@
-import { Body, Controller, Delete, Get, Injectable, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Injectable,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  ValidationPipe
+} from '@nestjs/common';
 import { BoardService } from './board.service';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
 
 @Controller('board')
+@ApiTags('Board')
 export class BoardController {
   constructor(
     private readonly boardService: BoardService
@@ -13,23 +28,23 @@ export class BoardController {
   }
 
   @Get(':id')
-  find(@Param('id') id: number) {
-    return this.boardService.findOne(Number(id));
+  find(@Param('id', ParseIntPipe) id: number) {
+    return this.boardService.findOne(id);
   }
 
   @Post()
-  create(@Body() data) {
+  create(@Body(new ValidationPipe()) data: CreateBoardDto) {
     return this.boardService.create(data);
   }
 
   @Put(":id")
-  update(@Param('id') id: number,
-         @Body() data) {
-    return this.boardService.update(Number(id), data);
+  update(@Param('id', ParseIntPipe) id: number,
+         @Body(new ValidationPipe()) data: UpdateBoardDto) {
+    return this.boardService.update(id, data);
   }
 
   @Delete(":id")
-  remove(@Param('id') id: number) {
-    return this.boardService.delete(Number(id));
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.boardService.delete(id);
   }
 }
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
 
 @Injectable()
 export class BoardService {
@@ -35,7 +37,7 @@ export class BoardService {
     return this.boards[index];
   }
 
-  create(data) {
+  create(data: CreateBoardDto) {
     const newBoard = {
       id: this.getNextId(),
       ...data
@@ -44,7 +46,7 @@ export class BoardService {
     return newBoard;
   }
 
-  update(id: number, data) {
+  update(id: number, data: UpdateBoardDto) {
     const index = this.getBoardId(id);
     if (index > -1) {
       this.boards[index] = {
