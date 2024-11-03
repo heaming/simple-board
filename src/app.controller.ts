55ea3294cfdad1581a6d4649ad8f798636a3ba6d
heaming <meetoohi@gmail.com>
2024-11-03 23:32:24 +0900

    [2024-11-03] configure 모듈 적용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Get, HttpException, HttpStatus, Logger, Query } from '@nest
 import { AppService } from "./app.service";
 import { Ip } from './decorators/ip.decorator';
 import * as http from 'node:http';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   private readonly logger = new Logger(); // DI 불가
 
@@ -16,6 +20,8 @@ export class AppController {
     // this.logger.error("ip:"+ip);
     // this.logger.verbose("ip:"+ip);
     // this.logger.warn("ip:"+ip);
+
+    console.log(this.configService.get<string>('ENVIRONMENT'))
     return this.appService.getHello();
     // throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
   }
